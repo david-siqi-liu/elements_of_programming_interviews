076e5530bb33af7864a4bcace57a087fadd1f8ff
David Liu <davidsiqiliu@gmail.com>
2020-02-25 09:38:16 -0500

    2/25/2020

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # Base cases
+    if y == 0:
+        return 1.0
+    elif y == 1:
+        return x
+
+    # If y is negative, x^y = (1/x)^(-y)
+    if y < 0:
+        return power(1.0 / x, -y)
+
+    # If y is even x^y = (x^(y/2))^2
+    if y & 1 == 0:
+        return power(x, (y >> 1)) ** 2
+    # If y is odd, x^y = x * (x^(y/2))^2
+    else:
+        return x * power(x, (y >> 1)) ** 2
 
 
 if __name__ == '__main__':
--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,9 +1,37 @@
 from test_framework import generic_test
 
 
+# def reverse(x: int) -> int:
+#     # If x is 0
+#     if x == 0:
+#         return 0
+#     # If x is negative, reverse its absolute value and add negative sign in front
+#     elif x < 0:
+#         return -1 * reverse(abs(x))
+#     # x is positive
+#     else:
+#         return int(str(x)[::-1])
+
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # Iteratively mod 10
+    result = 0
+    x_remaining = abs(x)
+
+    # Suppose input = -1132
+    # result initializes to 0, x_remaining initializes to 1132
+    # result increments by 1132 % 10 = 2, which is the most significant digit of x_remaining
+    # Now, result is 2, x_remaining is 113
+    # Next, result becomes 2 * 10 + 3 = 23, x_remaining becomes 11
+    # Then 231 and 1
+    # Then 2311 and 0 (since 1 // 10 = 0)
+    while x_remaining > 0:
+        result = result * 10 + x_remaining % 10
+        x_remaining = x_remaining // 10
+
+    if x < 0:
+        return -1 * result
+    else:
+        return result
 
 
 if __name__ == '__main__':
